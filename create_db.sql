e9b9fd495ec8387fabbf3397b97ef041af854939
Joylizzie <lizzievictory@yahoo.co.uk>
2021-03-26 20:36:25 -0700

    add syntax for killing process

--- create_db.sql
+++ create_db.sql
@@ -1,2 +1,11 @@
+-- kill all the other connections so that the database can be dropped.
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'ocean_stream' 
+  AND pid <> pg_backend_pid();
+
+
+
 drop database if exists ocean_stream;
 create database ocean_stream;
