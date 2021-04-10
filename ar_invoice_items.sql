a5078d79231b6360482aea7e947507870d4b92df
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-09 23:51:25 -0700

    delete redundant code

--- ar_invoice_items.sql
+++ ar_invoice_items.sql
@@ -16,14 +16,4 @@ insert into ar_invoice_item (company_code, rie_id, customer_id, general_ledger_n
                   and si.invoice_date between '2021-03-01' and '2021-03-31'
                   order by si.invoice_id;
                   
--- credit side                 
-insert into ar_invoice_item (company_code, rie_id, general_ledger_number, cc_id, currency_id, debit_credit, amount)
-    select ar.company_code, 
-            ar.rie_id,
-            501001 as general_ledger_number, 
-            1 as currency_id, 
-            'credit' as debit_credit,
-             as amount 
- from ar_invoice 
- group by company_code, transaction_date, customer_id, invoice_id, currency_id
- order by invoice_id;
+
