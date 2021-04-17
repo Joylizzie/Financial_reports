661fcdff36a2e1c50f47bfeafc2a165885606ef6
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-17 11:49:03 -0700

    delete renamed file

--- insert_ar_invoice.sql
+++
@@ -1,4 +0,0 @@
-insert into ar_invoice(company_code, date, invoice_id)
-	select company_code,invoice_date as date, invoice_id from sales_invoices
-	        where company_code = 'US001'
-	        and invoice_date between '2021-03-01' and '2021-03-31';
