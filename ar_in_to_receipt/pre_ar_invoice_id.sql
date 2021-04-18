bdfffec0c89cd1607cb4737a669e96ed14f85a7f
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-18 16:04:15 -0700

    update code for ar_invoice ar_receipt double entry postings

--- ar_in_to_receipt/pre_ar_invoice_id.sql
+++ ar_in_to_receipt/pre_ar_invoice_id.sql
@@ -1,3 +1,8 @@
+-- each sales_invoice will be followed by a ar_invoice double entry - ar_invoce
+-- generate ar_invoice id - rie_ids from below query
+-- after insert the query into database, rie_ids will be auto generated
+-- info of ar_invoice will feed the table ar_invoice_items
+
 insert into ar_invoice(company_code, date, invoice_id)
 	select company_code,invoice_date as date, invoice_id from sales_invoices
 	        where company_code = 'US001'
