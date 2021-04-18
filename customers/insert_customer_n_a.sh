d70eff7b230f2e190eb4a8ca1b1d733ee89605e0
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-18 00:29:54 -0700

    code for uploading customer_names addresses

--- customers/insert_customer_n_a.sh
+++ customers/insert_customer_n_a.sh
@@ -2,7 +2,7 @@
 
 cp data/customer_names.csv /tmp
 cp data/customer_addresses.csv /tmp
-psql --host=localhost -U joy2020 --dbname=pacific -a -f insert_customer_n_a.sql
+psql --host=localhost -U joy2020 --dbname=ocean_stream -a -f customers/insert_customer_n_a.sql
 
 
 
