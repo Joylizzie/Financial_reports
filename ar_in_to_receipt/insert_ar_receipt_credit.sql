f2337981a313a1773f75d78d55c823fdbf06c409
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-18 15:46:08 -0700

    remove redundant file

--- ar_in_to_receipt/insert_ar_receipt_credit.sql
+++
@@ -1,9 +0,0 @@
-
-set search_path TO ocean;
-
-
-COPY ar_receipt (company_code, entry_type_id, customer_id, transaction_date, rie_id, general_ledger_number,  currency_id, debit_credit, amount)
-FROM
-    '/tmp/ar_receipt_credit.csv' DELIMITER ',' CSV HEADER;
-
-
