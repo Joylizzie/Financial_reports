a4546407bb857c976f5675fa84194561d499315f
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-18 18:15:49 -0700

    tidy-up je to je folder

--- je_double_entries/insert_je_capital.sh
+++ je_double_entries/insert_je_capital.sh
@@ -1,17 +1,6 @@
 
 
 cp double_entries/je_item_capital.csv /tmp
-psql --host=localhost -U joy2020 --dbname=pacific -a -f insert_je_capital.sql
+psql --host=localhost -U joy2020 --dbname=ocean_stream -a -f insert_je_capital.sql
 
 
-
-# get filenames
-#IMPFILES=(data/customer_n_import/customer_names.csv, data/customer_n_import/customer_addresses.csv)
-
-# import the files
-#for i in ${IMPFILES[@]}
-#    do
-#        psql -U joy2020 -d pacific -c "\copy TABLE_NAME from '$i' DELIMITER ';' CSV HEADER"
-#        # move the imported file
- #       mv $i /data/customer_imported/
-#    done
