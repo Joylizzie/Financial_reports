09b4557b30643e05d2df841a038fd517981068ba
Henry <paulhtremblay@gmail.com>
2021-04-18 23:44:01 -0700

    fixing script

--- database/create_db_local.sh
+++ database/create_db_local.sh
@@ -1,7 +1,7 @@
 
 set -e
-psql --host=localhost -U joy2020 --dbname=postgres  -a -f create_db.sql
-psql --host=localhost -U joy2020  --dbname=ocean_stream -a -f create_table.sql
+psql --host=localhost -U ocean_user --dbname=postgres  -a -f database/create_db.sql
+psql --host=localhost -U ocean_user  --dbname=ocean_stream -a -f database/create_table.sql
 cp data/companies.csv /tmp
 cp data/business_type.csv /tmp
 cp data/entry_type.csv /tmp
@@ -15,4 +15,4 @@ cp data/products.csv /tmp
 cp data/tax.csv /tmp
 cp data/wbs.csv /tmp
 cp data/area_code.csv /tmp
-psql --host=localhost -U joy2020 --dbname=ocean_stream -a -f create_table_values.sql
+psql --host=localhost -U ocean_user --dbname=ocean_stream -a -f database/create_table_values.sql
--- run_all_shell_scripts.sh
+++ run_all_shell_scripts.sh
@@ -1,11 +1,12 @@
+set -e
 #!/bin/sh
 
-database/create_db_local.sh
-je_double_entries/insert_je_capital.sh
-customers/insert_customer_n_a.sh
-so_to_item/insert_so.sh
-so_to_item/insert_so_item.sh
-sales_invoice/insert_sales_invoices.sh
+bash database/create_db_local.sh
+bash je_double_entries/insert_je_capital.sh
+bash customers/insert_customer_n_a.sh
+bash so_to_item/insert_so.sh
+bash so_to_item/insert_so_item.sh
+bash sales_invoice/insert_sales_invoices.sh
 psql --host=localhost -U joy2020 --dbname=ocean_stream -a -f ar_in_to_receipt/pre_ar_invoice_id.sql
 psql --host=localhost -U joy2020 --dbname=ocean_stream -a -f ar_in_to_receipt/insert_ar_invoice_items_debit.sql
 ar_in_to_receipt/insert_ar_invoice_credit.sh
