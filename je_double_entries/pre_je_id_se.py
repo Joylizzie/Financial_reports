030dac96f5748d0ecffbe0aba9ffa5aaf5fe144e
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-19 23:11:55 -0700

    code for je postings

--- je_double_entries/pre_je_id_se.py
+++ je_double_entries/pre_je_id_se.py
@@ -1,41 +1,50 @@
 import os
-import datetime
 import psycopg2
 from psyco_conn_ocean_pw_pc import _get_conn, run_program
-from random_date import randomdate
 
 
+def _get_conn(pw, user_str):
+    conn = psycopg2.connect(host="localhost",
+                            database = db,
+                            user= user_str,
+                            password=pw)
+    conn.autocommit = False
+    return conn
 
 def generate_je_id(conn):
-    #retrieve je_id in the database
     sql_str = """ select nextval('journal_entry_je_id_seq')"""
     with conn.cursor() as curs:
         curs.execute(sql_str)  #cursor closed after the execute action
         (next_je_id) = curs.fetchone()# if the data is large, then use fetchone or fetchmany(), loop through
-    
     return [('US001', next_je_id)]
 
 # insert the generated value tuples into database
-def insert_values_tuples(conn, sql_insert):
+def insert_je_id(conn):
     tups = generate_je_id(conn)
-    with conn.cursor() as curs:
-        curs.execute(sql_insert, tups[0]) #cursor close after the execute
-    conn.commit()#commit the connection to confirm the insertion
-
-def do_work(conn, sql_insert):
-    cur = conn.cursor()
-    insert_values_tuples(conn, sql_insert)
-
-
-if __name__ == '__main__':    
-
-    # call the function with parameters to see if the function works# 
     sql_insert = '''insert into journal_entry (company_code, je_id)
                     VALUES(%s,%s)'''
+
+    try:
+        with conn.cursor() as curs:
+            curs.execute(sql_insert, tups[0]) #cursor close after the execute
+        conn.commit()#commit the connection to confirm the insertion
+    except (Exception, psycopg2.Error) as error:
+        print("Error while executing progamme", error)
+
+    finally:
+        # closing database connection.
+        if (conn):
+            conn.close()
+            print("PostgreSQL connection is closed")                        
+    
+
+if __name__ == '__main__':  
+    # call the function with parameters to see if the function works#
+    db = 'ocean_stream' 
     pw = os.environ['POSTGRES_PW']
     user_str = os.environ['POSTGRES_USER']
-    parms = {'sql_insert':sql_insert}
+    conn = _get_conn(pw, user_str)
+    insert_je_id(conn)
 
-    run_program(do_work, parms)
 
 
