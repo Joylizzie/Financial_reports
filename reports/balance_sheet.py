236c299e695605511f518adefdbb936b3fa55dce
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-21 23:41:42 -0700

    python and sql code for balance sheet preparing

--- reports/balance_sheet.py
+++ reports/balance_sheet.py
@@ -17,12 +17,17 @@ def _get_conn(pw, user_str):
 def get_t_list(conn, coacat_id_tup, start_date, end_date):
     sql_file = open('reports/t_list_asset.sql', 'r')
     sql = sql_file.read()
-    with conn.cursor() as curs:
-        curs.execute(sql, {'start_date':start_date, 'end_date':end_date, 'coacat_id_tup':coacat_id_tup})  #cursor closed after the execute action
-        t_list = curs.fetchall()
-    conn.commit
-    print(t_list)
-    return t_list 
+    try:
+        with conn.cursor() as curs:
+            curs.execute(sql, {'start_date':start_date, 'end_date':end_date, 'coacat_id_tup':coacat_id_tup})  #cursor closed after the execute action
+            t_list = curs.fetchall()
+        conn.commit()
+        #print(t_list)
+    finally:
+        conn.close()
+    return t_list
+   
+    
     
 # the tables in get_total_cost and get_total_expenses are not finalized yet and no data too. 
 '''
@@ -60,24 +65,52 @@ def get_total_expenses_api(conn, start_date, end_date):
     return total_expenses
 '''
 
-def bs(conn, coacat_id, start_date, end_date):
-    t = get_tlist(conn, start_date, end_date)
+def bs(conn, coacat_id_tup, start_date, end_date):
+    t = get_t_list(conn, coacat_id_tup, start_date, end_date)
     #cost = get_total_cost(conn, start_date, end_date)
     #expenses = get_total_expenses_api(conn, start_date, end_date)
     #total_expenses = sum(expense for (_,_,expense) in expenses)
     
     balance_at = end_date.strftime("%m") + "_" + end_date.strftime("%Y")
-    with open(os.path.join('reporting_results', f'bs_{out_file}.csv'), 'w') as bs:
-        name = 'Ocean Stream Balance Sheet ' 
+    with open(os.path.join('reporting_results', f'bs_{balance_at}.csv'), 'w') as bs:
+        name = 'Ocean Stream' 
         bs_writer = csv.writer(bs)   
         bs_writer.writerow([f'{name}\n\n'])
-        bs_writer.writerow(['',f'as of {end_date}'.rjust(15)])
-        for amount in t:
-            bs_writer.writerow(['amount', f'{t[8]:3,.2f}'.rjust(15)]) # number will be shown in 2 decimal
+        bs_writer.writerow([f'Balance Sheet\n\n'])
+        bs_writer.writerow([f'USD $\n\n'])
+        bs_writer.writerow(['','','',f'as of {end_date}'.rjust(15)])      
+        bs_writer.writerow([f'Assets\n\n'])
+        bs_writer.writerow(['',f'Current assets\n\n'])
+        bs_writer.writerow(['','',f'Cash and cash equilent'])
+        
+        bs_writer.writerow(['','',f'Total current assets'])
+        bs_writer.writerow([''])
+        bs_writer.writerow(['','',f'Property and Equipment'])
+        bs_writer.writerow([f'Total Assets'])
+        bs_writer.writerow([''])
+        
+        bs_writer.writerow([f'Liabilities\n\n'])
+        bs_writer.writerow(['',f'Current liabilities\n\n'])
+        bs_writer.writerow(['','',f'Accounts Payable'])
+        bs_writer.writerow(['','',f'Accured expenses'])
+        bs_writer.writerow(['','',f'Unearned revenue'])       
+        bs_writer.writerow(['',f'Total current liabilities'])
+        bs_writer.writerow([''])
+        bs_writer.writerow([f'Total Liabilities'])
+        bs_writer.writerow([''])
+        bs_writer.writerow([f'Shareholder\'s Equity']) 
+        bs_writer.writerow(['',f'Equity Capital']) 
+        bs_writer.writerow(['',f'Retained Earnings'])
+        bs_writer.writerow([f'Total Shareholder\'s Equity '])
+        bs_writer.writerow([f'Total Liabilities & Total Shareholder\'s Equity ']) 
+                 
+        # numeric format ['amount', f'{t[8]:3,.2f}'.rjust(15)]
+        #for row in t:
+            #bs_writer.writerow(row) # number will be shown in 2 decimal
         #pl_writer.writerow(['Cost', f'{cost:3,.2f}'.rjust(15)])
         #pl_writer.writerow(['Gross margin', f'{revenue - cost}'])
-        #for (gl_num, gl_name, expense) in expenses:
-         #   pl_writer.writerow([gl_name, f'{expense:3,.2f}'])
+        for (gl_num, gl_name, amount) in t:
+            bs_writer.writerow(['',gl_num,gl_name, f'{amount:3,.2f}'])
         #pl_writer.writerow(['total_expenses', total_expenses)])
         #pl_writer.writrow(['operating income', revenue - cost - total_expenses])
         print('balance sheet csv done writing')   
@@ -89,7 +122,8 @@ if __name__ == '__main__':
     pw = os.environ['POSTGRES_PW']
     user_str = os.environ['POSTGRES_USER']
     conn = _get_conn(pw, user_str)
-    coacat_id_tup = (2,3)
+    coacat_id_tup = (1,2,3,5,6)
     start_date = datetime.date(2021,3,1)
     end_date = datetime.date(2021,3,31)
-    get_t_list(conn, coacat_id_tup, start_date, end_date)
+    #get_t_list(conn, coacat_id_tup, start_date, end_date)
+    bs(conn, coacat_id_tup, start_date, end_date)
