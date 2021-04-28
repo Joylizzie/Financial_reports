6071a72573c478848a14c79c0d6d9154efee776a
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-27 20:23:16 -0700

    add a column

--- ar_in_to_receipt/insert_ar_invoice_credit.sql
+++ ar_in_to_receipt/insert_ar_invoice_credit.sql
@@ -2,7 +2,7 @@
 set search_path TO ocean;
 
 
-COPY ar_invoice_item (company_code, rie_id, general_ledger_number, cc_id,  currency_id, debit_credit, amount)
+COPY ar_invoice_item (company_code, rie_id, description,general_ledger_number, cc_id,  currency_id, debit_credit, amount)
 FROM
     '/tmp/pre_ar_invoices_credit_i_3.csv' DELIMITER ',' CSV HEADER;
 
