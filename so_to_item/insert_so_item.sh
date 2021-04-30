3b19b7c31594a03831c66ac96cdea5b30e5b9a81
Joylizzie <lizzievictory@yahoo.co.uk>
2021-04-29 22:09:44 -0700

    update code

--- so_to_item/insert_so_item.sh
+++ so_to_item/insert_so_item.sh
@@ -3,4 +3,4 @@ set -e
 
 cp intermediate_csv/pre_sales_orders_items_i.csv /tmp
 
-psql --host=localhost -U ocean_user --dbname=pacific -a -f so_to_item/insert_so_item.sql
+psql --host=localhost -U ocean_user --dbname=ocean_stream -a -f so_to_item/insert_so_item.sql
